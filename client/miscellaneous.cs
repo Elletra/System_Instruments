99014b338747e5de980f0d1bde6dd3c3ef397310
Electrk <Electrk@ymail.com>
2018-05-22 22:10:10 -0700

    Added "Copy" button to InstrumentsEditTextDlg

--- client/miscellaneous.cs
+++ client/miscellaneous.cs
@@ -18,11 +18,13 @@ function InstrumentsClient::setGuiPanel(%this, %panel) {
   }
 }
 
+// Not my best idea, in retrospect...
 function InstrumentsClient::openEditTextDialog(%this, %title, %label, %editVar, %editCmd, %btnText, %btnCmd, %footer) {
   InstrumentsEditTextDlg_Window.setText(%title);
   InstrumentsEditTextDlg_Label.setText("<font:impact:18>" @ %label @ ": ");
   InstrumentsEditTextDlg_Button.setText(%btnText);
   InstrumentsEditTextDlg_BottomText.setText(%footer);
+  InstrumentsEditTextDlg_BottomText._centerX(-50);
 
   InstrumentsEditTextDlg_TextEdit.variable = %editVar;
   InstrumentsEditTextDlg_TextEdit.command = %editCmd;
@@ -30,6 +32,13 @@ function InstrumentsClient::openEditTextDialog(%this, %title, %label, %editVar, 
   InstrumentsEditTextDlg_Button.command = "Canvas.popDialog(InstrumentsEditTextDlg); " @ %btnCmd;
 
   Canvas.pushDialog(InstrumentsEditTextDlg);
+}
+
+function InstrumentsClient::copyEditText(%this) {
+  setClipboard(InstrumentsEditTextDlg_TextEdit.getValue());
+
+  InstrumentsEditTextDlg_Copy.disable();
+  InstrumentsEditTextDlg_Copy.schedule(500, enable);
 }
 
 function InstrumentsClient::canPlayLive(%this) {
