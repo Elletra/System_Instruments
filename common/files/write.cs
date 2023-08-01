debe6982ba8e93176cc6de86a8c770eb713788bc
Electrk <Electrk@ymail.com>
2023-08-01 02:17:49 -0700

    Consolidate `FileObject` write functions to `instrFileWrite()`

--- common/files/write.cs
+++ common/files/write.cs
@@ -1,41 +1,38 @@
-function FileObject::instrWriteHeader (%this)
+function FileObject::instrFileWrite (%this)
 {
-	%this.writeLine("BLM " @ %this.instrFileVersion
-		SPC %this.instrFileNotationVersion
-		SPC %this.instrFileType
-		SPC %this.instrFileInstrument
-	);
+	%fileType = %this.instrFileType;
 
+	%this.writeLine("BLM " @ %this.instrFileVersion SPC %this.instrFileNotationVersion SPC %fileType);
 	%this.writeLine(%this.instrFileCredits);
 	%this.writeLine(%this.instrFileUploader);
-}
 
-function FileObject::instrWritePattern (%this)
-{
-	%this.instrWriteHeader();
-	%this.writeLine(%this.instrFilePattern);
-}
-
-function FileObject::instrWriteSong (%this)
-{
-	%this.instrWriteHeader();
-	%this.writeLine(%this.instrFileSong);
-
-	//* We don't need to write a bunch of blank lines if we don't have to, so let's just get the
-	//  index of the final actual song pattern. *//
-
-	%max = 0;
-
-	for (%i = 0; %i < $Instruments::Max::SongPatterns; %i++)
+	switch (%fileType)
 	{
-		if (%this.instrFileSongPattern[%i] !$= "")
-		{
-			%max = %i;
-		}
-	}
+		case $Instruments::FileType::Pattern:
+			%this.writeLine(%this.instrFilePattern);
 
-	for (%i = 0; %i <= %max; %i++)
-	{
-		%this.writeLine(%this.instrFileSongPattern[%i]);
+		case $Instruments::FileType::Song:
+			%this.writeLine(%this.instrFileSong);
+
+			//* We don't need to write a bunch of blank lines if we don't have to, so let's just get the
+			//  index of the final actual song pattern. *//
+
+			%max = 0;
+
+			for (%i = 0; %i < $Instruments::Max::SongPatterns; %i++)
+			{
+				if (%this.instrFileSongPattern[%i] !$= "")
+				{
+					%max = %i;
+				}
+			}
+
+			for (%i = 0; %i <= %max; %i++)
+			{
+				%this.writeLine(%this.instrFileSongPattern[%i]);
+			}
+
+		default:
+			error("Invalid file type '", %fileType, "'");
 	}
 }
